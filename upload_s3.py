036022f744a7f613d86e313dd2012797a843c829
Astghik Manvelyan <astghik.manvelyan@gmail.com>
2019-04-18 15:01:42 +0400

    editing

--- upload_s3.py
+++ upload_s3.py
@@ -6,6 +6,7 @@ def main():
     ACCESS_KEY_ID = ''
     ACCESS_SECRET_KEY = ''
 
+
     s3 = boto3.client('s3',
                       aws_access_key_id=ACCESS_KEY_ID,
                       aws_secret_access_key=ACCESS_SECRET_KEY)
