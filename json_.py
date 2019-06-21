55cbe24043ae2feda286b246923fd8f2b8708885
Astghik Manvelyan <astghik.manvelyan@gmail.com>
2019-06-21 11:02:13 +0400

    modifying

--- json_.py
+++ json_.py
@@ -2,11 +2,12 @@ import subprocess
 import json
 import os
 
-file = os.path.join(os.getcwd(), "session_file.json")
 
-subprocess.run("sed -i 's/OFFLOAD_END_PLACEHOLDER/date/' session_file.json", shell=True, check=True)
-subprocess.run("sed -i 's/INPROGRESS/OK/' session_file.json", shell=True, check=True)
-subprocess.run("sed -i '/components/r compoent_file.json' session_file.json", shell=True, check=True)
-subprocess.run("sed -i '/COMPONENTSPLACEHOLDER/d' session_file.json", shell=True, check=True)
+subprocess.run("""sed -i "s/OFFLOAD_END_PLACEHOLDER/$(date '+%Y-%m-%d')/g" session_file.json""", shell=True, check=True)
+subprocess.run("sed -i 's/INPROGRESS/OK/g' session_file.json", shell=True, check=True)
+subprocess.run("""sed -i "s/COMPONENTSPLACEHOLDER/$(echo $(cat compoent_file.json))/g" session_file.json""", shell=True, check=True)
+
+
+
 
 
